3d793d7bd2220a27cd99a8acdc9dc30e99b0cd98
WJC <wj.colley@hotmail.co.uk>
2021-07-14 14:29:38 +0100

    Refactored Dockerfiles and bash script

--- dockerfiles/run.sh
+++ dockerfiles/run.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-newData=`curl -s https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson`
-echo $newData | jq '.features[]' | jq '[.properties.place, .geometry.coordinates[0,1], .properties.mag]' | jq '((.[2]|tostring) + "|" + (.[1]|tostring) + "|" + .[0] + "|" + (.[3]|tostring))' | jq -r . > hour.chi
-./cliapp -i hour.chi --dataset-name 'all_hour'
-rm hour.chi
-echo "Dataset 'all_hour' created."
+create_dataset() {
+    echo `curl -s https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/$1.geojson` | jq '.features[]' | jq '[.properties.place, .geometry.coordinates[0,1], .properties.mag]' | jq '((.[2]|tostring) + "|" + (.[1]|tostring) + "|" + .[0] + "|" + (.[3]|tostring))' | jq -r . > hour.chi
+    ./cliapp -i hour.chi --dataset-name $1
+    rm hour.chi
+    echo "Dataset '$1' created."
+}
 
-newData=`curl -s https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson`
-echo $newData | jq '.features[]' | jq '[.properties.place, .geometry.coordinates[0,1], .properties.mag]' | jq '((.[2]|tostring) + "|" + (.[1]|tostring) + "|" + .[0] + "|" + (.[3]|tostring))' | jq -r . > hour.chi
-./cliapp -i hour.chi --dataset-name 'all_day'
-rm hour.chi
-echo "Dataset 'all_day' created."
-
-newData=`curl -s https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson`
-echo $newData | jq '.features[]' | jq '[.properties.place, .geometry.coordinates[0,1], .properties.mag]' | jq '((.[2]|tostring) + "|" + (.[1]|tostring) + "|" + .[0] + "|" + (.[3]|tostring))' | jq -r . > hour.chi
-./cliapp -i hour.chi --dataset-name 'all_week'
-rm hour.chi
-echo "Dataset 'all_week' created."
+create_dataset 'all_hour'
+create_dataset 'all_day'
+create_dataset 'all_week'
