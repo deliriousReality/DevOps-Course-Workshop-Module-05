ae622fdfb8602f923207d2593dfc8485e7c55aa3
WJC <wj.colley@hotmail.co.uk>
2021-07-19 20:22:36 +0100

    Redis added

--- dockerfiles/run.sh
+++ dockerfiles/run.sh
@@ -2,7 +2,7 @@
 
 create_dataset() {
     echo `curl -s https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/$1.geojson` | jq '.features[]' | jq '[.properties.place, .geometry.coordinates[0,1], .properties.mag]' | jq '((.[2]|tostring) + "|" + (.[1]|tostring) + "|" + .[0] + "|" + (.[3]|tostring))' | jq -r . > hour.chi
-    ./cliapp -i hour.chi --dataset-name $1
+    ./cliapp -i hour.chi --dataset-name $1 -r
     rm hour.chi
     echo "Dataset '$1' created."
 }
